9a43295911924d441f87ee813ca2cf21b026e02d
Alexis Tercero <alexistercero55@gmail.com>
2023-02-20 00:14:40 -0600

    TASK | Good css setup.

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // Redux Toolkit setup
 import { store } from './store/store';
 import { Provider } from 'react-redux';
