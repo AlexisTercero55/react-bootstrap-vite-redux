203a781e52ca842748aa10e8582faeacc535a44d
Alexis Tercero <alexistercero55@gmail.com>
2023-02-19 21:40:09 -0600

    CHANGE | Counter App saved as CounterApp.jsx uwu

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './apps/CounterApp'
 //import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Redux Toolkit setup
