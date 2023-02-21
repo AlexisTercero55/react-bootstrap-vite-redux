81d596869f5d5acddb602455cf75e98bf1aa7294
Alexis Tercero <alexistercero55@gmail.com>
2023-02-21 13:39:20 -0600

    TASK | CSS grid of cards and hover grid effect

--- src/components/CardWithCanvas.jsx
+++ src/components/CardWithCanvas.jsx
@@ -1,8 +1,15 @@
+/** 2/20/23 : CDMX/México.
+ * @author  @AlexisTercero55
+ * @github  https://github.com/AlexisTercero55
+ * @mail    alexistercero55@gmail.com
+ * @license MPL-2.0
+ */
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { useFrame } from '@react-three/fiber';
-import { useRef, Suspense } from 'react';
+import { useRef, Suspense, useState } from 'react';
 import { Model } from './My_animated_mewtwo';
+import '../css/CardWithCanvas.css';
 
 function Box() {
   const meshRef = useRef();
@@ -21,28 +28,25 @@ function Box() {
 }
 
 export default function CardWithCanvas() {
-  const canvasRef = useRef();
+  const [pokeName, setpokeName] = useState('Pokemon');
   
   return (
     <div className="card">
       <div className="card-border"></div>
-      <div className="card-description"
-        style={{zIndex:4, color:'#f5f5f5'}}>
-        <h3>Card Title</h3>
-        <p>Description text goes here.</p>
+      <div className="card-description">
+        <h3>{pokeName}</h3>
+        <p>Description text.</p>
       </div>
       <div className="card-content">
-        <Canvas className='Canvas' ref={canvasRef}>
+        <Canvas className='Canvas'>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          {/* <Box /> */}
           <Suspense>
             <Model></Model>
           </Suspense>
           <OrbitControls enablePan={true} 
                         enableZoom={true}
                         enableRotate={true}
-                        domElement={canvasRef.current}
           />
         </Canvas>
       </div>
